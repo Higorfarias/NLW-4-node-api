67117a2e1c57baa0bbbafac6477d155921b5ef94
Higor <higornf2009@gmail.com>
2021-03-01 19:45:03 -0300

    Get and Post

--- src/server.ts
+++ src/server.ts
@@ -9,5 +9,6 @@ app.get("/", (request, response) => {
 app.post("/", (request, response) => {
     return response.json({message: "Os dados foram salvos com sucesso!"})
 })
+
 // npm insttal @types/express -D
 app.listen(3333, () => console.log("Server is running!"));
